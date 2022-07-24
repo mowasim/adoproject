1ee5c855900df504ad974ac9f3da0f54da51c8f7
zam dev <terraform_devops1@hotmail.com>
2022-07-24 00:06:02 +0000

    Updated Program.cs

--- webapp/Program.cs
+++ webapp/Program.cs
@@ -10,6 +10,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    //new commit
     app.UseHsts();
 }
 
